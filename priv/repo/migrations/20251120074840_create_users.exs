f1e4bf4f54e2b262ffaee3c064834758350ba9d6
sliman.ibrahim <sliman.ibrahim@hiast.edu.sy>
2025-11-20 02:53:18 -0500

    add the first migration

---
+++ priv/repo/migrations/20251120074840_create_users.exs
@@ -0,0 +1,13 @@
+defmodule SimpleEctoApp.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table( :users) do
+      add :name, :string
+      add :email, :string
+      add :age, :integer
+
+      timestamps()
+    end
+  end
+end
